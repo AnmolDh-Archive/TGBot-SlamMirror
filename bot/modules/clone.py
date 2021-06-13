ee4787c1f2e3a133bfb8df145fdf79562e4e0426
Anmol Dhiman <73706789+Anmoldh@users.noreply.github.com>
2021-06-13 14:46:34 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -61,7 +61,7 @@ def cloneNode(update, context):
             else:
                 uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
             if uname is not None:
-                cc = f'\n\ncc: {uname}'
+                cc = f'\n\n𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐫: <i>{uname}</i>'
             sendMarkup(result + cc, context.bot, update, button)
     else:
         sendMessage('Provide G-Drive Shareable Link to Clone.', context.bot, update)
